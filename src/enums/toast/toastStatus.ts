95fc4c59979a90b59bc6022cab2bb0385aa249fd
Oleksandra-Masiuk <99670702+Oleksandra-Masiuk@users.noreply.github.com>
2023-06-12 10:09:53 +0300

    created enum for toast status

---
+++ src/enums/toast/toastStatus.ts
@@ -0,0 +1,5 @@
+enum ToastStatus {
+  SUCCESS = 'success',
+}
+
+export {ToastStatus};
--- src/services/ToastServise.ts
+++ src/services/ToastServise.ts
@@ -1,9 +1,10 @@
 import Toast from 'react-native-toast-message';
 import {ToastText} from '../enums/toast/toastText';
+import {ToastStatus} from '../enums/toast/toastStatus';
 
 const successToast = (text: string) =>
   Toast.show({
-    type: 'success',
+    type: ToastStatus.SUCCESS,
     text1: text,
   });
 
